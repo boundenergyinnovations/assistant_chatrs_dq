31e92241dde806f74c329eea91498ec07fc13c54
boundenergyinnovations <boundenergyinnovations@gmail.com>
2024-09-07 15:39:10 -0500

    Add files via upload

--- setup_chatrs_server.sh
+++ setup_chatrs_server.sh
@@ -41,4 +41,19 @@ sudo nginx -t
 # Reload Nginx to apply changes
 sudo systemctl reload nginx
 
+wget -O Cargo.toml https://raw.githubusercontent.com/boundenergyinnovations/assistant_chatrs_dq/main/Cargo.toml
+mkdir src
+cd src
+wget -O main.rs https://raw.githubusercontent.com/boundenergyinnovations/assistant_chatrs_dq/main/src/main.rs
+wget -O routes.rs https://raw.githubusercontent.com/boundenergyinnovations/assistant_chatrs_dq/main/src/routes.rs
+wget -O openai.rs https://raw.githubusercontent.com/boundenergyinnovations/assistant_chatrs_dq/main/src/openai.rs
+cd ..
+mkdir static
+cd static
+wget -O index.html https://raw.githubusercontent.com/boundenergyinnovations/assistant_chatrs_dq/main/static/index.html
+wget -O index_code.html https://raw.githubusercontent.com/boundenergyinnovations/assistant_chatrs_dq/main/static/index_code.html
+wget -O script.js https://raw.githubusercontent.com/boundenergyinnovations/assistant_chatrs_dq/main/static/script.js
+wget -O styles.css https://raw.githubusercontent.com/boundenergyinnovations/assistant_chatrs_dq/main/static/styles.css
+cd ..
+wget -O chatrs_dq https://raw.githubusercontent.com/boundenergyinnovations/assistant_chatrs_dq/main/releases/chatrs_dq
 
